dca565db58b63954dd71fe2245cc391a14d9b5ce
Rauulito <7rnavarro7@gmail.com>
2021-10-27 12:18:48 +0200

    realizado

--- Ejercicio-1.py
+++ Ejercicio-1.py
@@ -7,3 +7,14 @@ velocidad = float(input())
 distancia = velocidad * tiempo
 
 print("La distancia recorrida por el móvil es: " + str(distancia) + " metros")
+
+# Pseudocódigo
+# 1. Variables: velocidad, tiempo, distancia
+# 2. Inicio
+# 3. Escribir("Ingrese el tiempo en segundos:")
+# 4. Leer (tiempo)
+# 5. Escribir("Ingrese la velocidad:")
+# 6. Leer (distancia)
+# 7. distancia = velocidad * tiempo
+# 8. Escribir("La distancia recorrida por el móvil es: " + str(distancia) + " metros")
+# 9. Fin
