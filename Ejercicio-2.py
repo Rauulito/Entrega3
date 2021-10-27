0bc9072924be75bd9e644c72c6202b8a0fb238e5
Rauulito <7rnavarro7@gmail.com>
2021-10-27 12:36:05 +0200

    realizado

--- Ejercicio-2.py
+++ Ejercicio-2.py
@@ -1,3 +1,16 @@
+# Pseudocódigo
+# 1. Variables: N1(nota primer parcial), N2(nota segundo parcial), N1(nota tercer parcial)
+# 2. Inicio
+# 3. Escribir("Ingrese la nota del primer parcial:")
+# 4. Leer (N1)
+# 5. Escribir("Ingrese la nota del segundo parcial:")
+# 6. Leer (N2)
+# 7. Escribir("Ingrese la nota del tercer parcial:")
+# 8. Leer (N3)
+# 9. promedioSimple = (N1 * N2 *N3)/ 3
+# 10. Escribir("La media de las notas parciales es : " + str(promedioSimple))
+# 11. Fin
+
 print("Ingrese la nota del primer parcial:")
 N1 = float(input())
 
@@ -11,15 +24,3 @@ promedioSimple = (N1 + N2 + N3) / 3
 
 print("La media de las notas parciales es: " + str(promedioSimple))
 
-# Pseudocódigo
-# 1. Variables: N1(nota primer parcial), N2(nota segundo parcial), N1(nota tercer parcial)
-# 2. Inicio
-# 3. Escribir("Ingrese la nota del primer parcial:")
-# 4. Leer (N1)
-# 5. Escribir("Ingrese la nota del segundo parcial:")
-# 6. Leer (N2)
-# 7. Escribir("Ingrese la nota del tercer parcial:")
-# 8. Leer (N3)
-# 9. promedioSimple = (N1 * N2 *N3)/ 3
-# 10. Escribir("La media de las notas parciales es : " + str(promedioSimple))
-# 11. Fin
