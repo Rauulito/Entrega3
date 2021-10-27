ed50c22175c0619ac81188d815a7eb82178ee1f7
Rauulito <7rnavarro7@gmail.com>
2021-10-27 11:36:05 +0200

    realizado

---
+++ Ejercicio-2.py
@@ -0,0 +1,12 @@
+print("Ingresa la primera nota:")
+N1 = float(input())
+
+print("Ingresa la segunda nota:")
+N2 = float(input())
+
+print("Ingresa la tercera nota:")
+N3 = float(input())
+
+promedioSimple = (N1 + N2 + N3) / 3
+
+print("La media de las notas parciales es: " + str(promedioSimple))
