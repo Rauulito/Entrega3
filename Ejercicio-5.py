950ce23fa87f3f2b63ab36475d74ed5192e34c99
Rauulito <7rnavarro7@gmail.com>
2021-10-27 12:46:17 +0200

    realizado

--- Ejercicio-5.py
+++ Ejercicio-5.py
@@ -1,3 +1,13 @@
+# Pseudocódigo
+# 1. Variables: cantidadGasolina, precioGasolinaPorLitro=4.5
+# 2. Inicio
+# 3. Escribir("Ingrese lo que "surten" en galones,es decir, la cantidad de gasolina :")
+# 4. Leer (cantidadGasolina)
+# 5. pagoCliente = cantidadGasolina* precioGasolinaPorLitro
+# 6. Escribir("El dinero que paga el cliente es": " + str(pagoCliente))
+# 7. Fin
+
+
 print("Lo que surgen en galones")
 cantidadGasolina= float(input())
 
