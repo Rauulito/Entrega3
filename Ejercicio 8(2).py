1abaa836d12252431f218801b046a3ba7e4eb14b
Rauulito <7rnavarro7@gmail.com>
2021-10-28 00:29:02 +0200

    realizado

---
+++ Ejercicio 8(2).py
@@ -0,0 +1,21 @@
+import random
+
+#Numero aleatorio del 1 al 300
+numeroAleatorio= random.randint(1,300)
+
+contador_intentos= 0
+
+while (numeroUsuario != numeroAleatorio and contadorIntentos<9):
+    numeroUsuario= int(input("Ingresa un numero entre 1 y 300:"))
+    if numeroAleatorio < numeroUsuario:
+        print("Escribe un numero más pequeño")
+    if numeroAleatorio > numeroUsuario:
+        print("Escribe un numero más grande")
+    else:
+        break
+    contadorIntentos= contadorIntentos +1
+
+if numeroUsuario == numeroAleatorio
+   print("El número acertado es:" + str(numeroAleatorio) + "en" + str(contadorIntentos))
+else:
+    print("No has acertado el número y se te han acabado los intentos")
