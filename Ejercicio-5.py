8d5f6312af77e0c6dcb0f77b1db9655580ac9754
Rauulito <7rnavarro7@gmail.com>
2021-10-27 12:06:32 +0200

    realizado

---
+++ Ejercicio-5.py
@@ -0,0 +1,8 @@
+print("Lo que surgen en galones")
+cantidadGasolina= float(input())
+
+precioGasolinaPorLitro= 4,5
+
+pagoCliente= cantidadGasolina* precioGasolinaPorLitro
+
+print("El dinero que paga el cliente es:" +str(pagoCliente5))
