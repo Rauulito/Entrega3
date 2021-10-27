a446a7dcc85383680b04ab1b6ead5b9165f601ca
Rauulito <7rnavarro7@gmail.com>
2021-10-27 12:35:24 +0200

    realizado

--- Ejercicio-3.py
+++ Ejercicio-3.py
@@ -1,3 +1,16 @@
+# Pseudocódigo
+# 1. Variables: respuestasCorrectas, respuestasIncorrectas, respuestasEnBlanco
+# 2. Inicio
+# 3. Escribir("Ingrese el número de respuestas correctas:")
+# 4. Leer (respuestasCorrectas)
+# 5. Escribir("Ingrese el número de respuestas incorrectas:")
+# 6. Leer (respuestasIncorrectas)
+# 7. Escribir("Ingrese el número de respuestas en blanco:")
+# 8. Leer (respuestasEnBlanco)
+# 9. puntajeFinal = respuestasCorrectas*3 + respuestasIncorrectas*-1 + respuestasEnBlanco*0
+# 10. Escribir("La puntuación final es : " + str(puntajeFinal))
+# 11. Fin
+
 print("Número de respuestas correctas")
 respuestasCorrectas= float(input())
 
@@ -9,3 +22,4 @@ respuestasEnBlanco= float(input())
 
 puntajeFinal= respuestasCorrectas*3 + respuestasIncorrectas*-1 + respuestasEnBlanco*0  
 print("La puntuación final es:" +str(puntajeFinal))
+
