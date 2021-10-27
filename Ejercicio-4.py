65957af9c81481e7e6e54286f3944fd9bafba147
Rauulito <7rnavarro7@gmail.com>
2021-10-27 12:42:42 +0200

    realizado

--- Ejercicio-4.py
+++ Ejercicio-4.py
@@ -1,3 +1,16 @@
+# Pseudocódigo
+# 1. Variables: partidosGanados, partidosEmpatados, partidosPerdidos
+# 2. Inicio
+# 3. Escribir("Ingrese el número de partidos ganados:")
+# 4. Leer (partidosGanados)
+# 5. Escribir("Ingrese el número de partidos empatados:")
+# 6. Leer (partidosEmpatados)
+# 7. Escribir("Ingrese el número de partidos perdidos:")
+# 8. Leer (partidosPerdidos)
+# 9. puntajeFinal = partidosGanados*3 + partidosEmpatados*1 + partidosPerdidos*0
+# 10. Escribir("La puntuación final es : " + str(puntajeFinal))
+# 11. Fin
+
 print("Número de partidos ganados")
 partidosGanados= float(input())
 
