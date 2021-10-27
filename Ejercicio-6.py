c994b563a020f4e544662d9f4b40010e4e345113
Rauulito <7rnavarro7@gmail.com>
2021-10-27 13:09:17 +0200

    realizado

---
+++ Ejercicio-6.py
@@ -0,0 +1,19 @@
+print("El primer numero entero es:")
+a = int ( input ())
+print("El segundo numero entero es:5")
+b = int ( input ())
+
+suma= a+b
+print("La suma es:"+str(suma))
+
+resta= a-b
+print("La resta es:"+str(resta))
+
+producto= a*b
+print("El producto es:"+str(producto))
+
+divisionEnteros= a//b
+print("La division de enteros es:"+str(divisionEnteros))
+
+divisionFlotante= a/b
+print("La division flotante es:"+str(divisionFlotante))
