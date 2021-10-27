2c13092e0834775481e1806f1ad0b04d2a11244f
Rauulito <7rnavarro7@gmail.com>
2021-10-27 11:50:28 +0200

    realizado

--- Ejercicio-3.py
+++ Ejercicio-3.py
@@ -7,6 +7,5 @@ respuestasIncorrectas= float(input())
 print("Número de respuestas en blanco")
 respuestasEnBlanco= float(input())
 
-puntajeFinal= respuestasCorrectas*3 + respuestasIncorrectas*-1  #respuestas en blanco al ser por 0 no va a variar el resultado, por lo tanto no hace falta ponerlas
-
+puntajeFinal= respuestasCorrectas*3 + respuestasIncorrectas*-1 + respuestasEnBlanco*0  
 print("La puntuación final es:" +str(puntajeFinal))
